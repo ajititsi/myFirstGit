4a0e494d975d1459af5c93961eddbd8429d0d01a
ajititsi <ajititsi@yahoo.com>
2018-01-14 15:56:23 +0530

    check the email message

--- MyFirst.java
+++ MyFirst.java
@@ -3,5 +3,6 @@ class MyFirst
 	public static void main(String [] args)
 	{
 		System.out.println("This is what I want to display");
+		System.out.println("Check the Email notification from github");
 	}//end of the main method
 }//End of my clss
