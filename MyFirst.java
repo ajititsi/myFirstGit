e8e281c29c307c928f1b2df220095bd64675d07a
ajititsi <ajititsi@yahoo.com>
2018-01-15 17:13:12 +0530

    Changes a bit

--- MyFirst.java
+++ MyFirst.java
@@ -2,7 +2,7 @@ class MyFirst
 {
 	public static void main(String [] args)
 	{
-		System.out.println("This is what I want to display");
-		System.out.println("Check the Email notification from github");
+		System.out.println("This is what I want to display"); 
+		System.out.println("Check the Email notification from test github");
 	}//end of the main method
 }//End of my clss
