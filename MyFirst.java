e5db245d27b77e8bdae33f76e0ba20c17f582d20
ajititsi <ajititsi@yahoo.com>
2018-08-14 19:05:37 +0530

    modified

--- MyFirst.java
+++ MyFirst.java
@@ -5,4 +5,4 @@ class MyFirst
 		System.out.println("This is what I want to display"); 
 		System.out.println("Check the Email notification from test github");
 	}//end of the main method
-}//End of my clss
+}//End of my class
